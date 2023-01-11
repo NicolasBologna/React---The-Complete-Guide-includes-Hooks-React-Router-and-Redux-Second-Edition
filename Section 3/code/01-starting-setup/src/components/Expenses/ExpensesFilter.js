04ba9f4f0790b29fed669021622defbcf1d6aed1
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2023-01-11 09:10:11 -0300

    finish Chapter 5 - Outputting Conditional Content

---
+++ Section 3/code/01-starting-setup/src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,21 @@
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const handleYearChange = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={handleYearChange} value={props.selected}>
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
